e304f5636998de328dd7081cd6a6113cd8b8d17f
Babar <babarnadeem996@gmail.com>
2026-09-14 12:10:10 +0500

    project structure ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Babar",
+    author_email="babarnadeem996@gmail.com",
+    packages=find_packages()
+)
