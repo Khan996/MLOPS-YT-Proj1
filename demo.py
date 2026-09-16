17673787dd95d79d3485db3bdd24559739a3b6ea
Babar <babarnadeem996@gmail.com>
2026-09-16 21:14:58 +0500

    EDA and feature engineering notebook added

--- demo.py
+++ demo.py
@@ -8,14 +8,16 @@
 # logging.error("This is an ERROR message")
 # logging.critical("This is a CRITICAL message")
 
+#------------------------------------------------
+
 # below code is to check the exception file 
 
-from src.exception import MyException
-from src.logger import logging 
-import sys
+# from src.exception import MyException
+# from src.logger import logging 
+# import sys
 
-try:
-    a = 1 +'Z'
-except Exception as e:
-    logging.info(e)
-    raise MyException(e, sys) from e 
+# try:
+#     a = 1 +'Z'
+# except Exception as e:
+#     logging.info(e)
+#     raise MyException(e, sys) from e 
